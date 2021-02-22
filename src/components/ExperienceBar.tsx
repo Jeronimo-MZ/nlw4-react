7d631725c617a53575dc1f0ceebd90e3e72f388d
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-02-23 01:45:45 +0200

    Dia 1

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,17 @@
+// export interface ExperienceBarProps {
+    
+// }
+ 
+export const ExperienceBar = () => {
+    return ( 
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{width: '50%'}} />
+                <span className="current-experience"> 300 xp</span>
+            </div>
+            <span>600 xp</span>
+        </header>
+     );
+}
+ 
