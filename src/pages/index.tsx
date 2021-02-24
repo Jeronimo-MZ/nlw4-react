2bc7c2e5c0fd6fc1151c518d3b9935b2ab958ea4
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-02-24 03:19:26 +0200

    add Countdown (Fim do dia 2)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
 import { ExperienceBar } from "../components/ExperienceBar";
-import Head from 'next/head';
 import { Profile } from "../components/Profile";
 import styles from './../styles/pages/Home.module.css';
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges/>
+          <Countdown />
         </div>
         <div>
 
