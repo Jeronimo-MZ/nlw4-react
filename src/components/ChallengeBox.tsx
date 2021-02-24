5d6cd53f95b6f4d8a60d3f3dfd45059b69f1650e
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-02-24 20:50:23 +0200

    Corrigindo erro de digitação

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -42,7 +42,7 @@ export function ChallengeBox () {
                         <strong>Finalize um ciclo para receber um desafio</strong>
                         <p>
                             <img src="/icons/level-up.svg" alt="Level Up"/>
-                            Avance de level completendo desafios
+                            Avance de level completando desafios
                         </p>
                     </div>
                 )}
